819ef167047a8d73db60ea231d6b82b1d8de7376
p4co <rungr4vity@gmail.com>
2025-03-10 00:55:05 -0600

    Kotlin certification exe 1

---
+++ app/src/main/java/il/pacolo/com/mymodules/playgrounds/myplay.kt
@@ -0,0 +1,34 @@
+package il.pacolo.com.mymodules.playgrounds
+
+import android.icu.text.ListFormatter.Width
+
+
+fun main() {
+    
+}
+
+
+// certification problems
+//Task: Write a function that takes a nullable String and returns the length of the string. If the input is null, return -1.
+
+
+fun getLength(input:String?): Int = input?.length ?: -1
+
+//Task: Create a sealed class Shape with two subclasses:
+//
+//Circle(radius: Double)
+//Rectangle(length: Double, width: Double)
+//Write a function that takes a Shape and calculates its area.
+
+
+sealed class Shape() {
+    data class Circle( val radius:Double): Shape()
+    data class Rectangle(val lenght:Double,val width: Double): Shape()
+}
+
+fun calculateArea(shape:Shape): Double {
+    return when(shape) {
+        is Shape.Circle ->  Math.PI * shape.radius * shape.radius
+        is Shape.Rectangle ->  shape.lenght * shape.width
+    }
+}
